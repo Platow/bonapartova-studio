8feef713793accd54551b19206a137ffa7b4c9cd
Nikita Lutskiy <tigrash8@mail.ru>
2021-04-08 21:54:02 +0300

    Add redirect to admin. Add secure cookies settings

--- saloon/saloon/urls.py
+++ saloon/saloon/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 admin.site.site_header = "Администрация Bonapartova Studio"
 
+def redirect_view(request):
+    return redirect('/admin')
+
 urlpatterns = [
+    path('', redirect_view),
     path('admin/', admin.site.urls),
 ]
 
